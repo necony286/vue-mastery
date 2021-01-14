91e0dc51280d008085cdb422f0326f4ff76977f7
necony286 <necony286@gmail.com>
2021-01-14 21:35:18 +0100

    l5 start

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      //   inStock: true,
-      invetory: 8,
-      // challange
-      onSale: true,
-    };
-  },
-});
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
+        }
+    }
+})
