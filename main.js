6b5c300dc5a58f02b71ac0b43c4e25987633b279
necony286 <necony286@gmail.com>
2021-01-14 21:23:30 +0100

    l3 finished

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      //   challange
+      link: "https://necony286.com",
+    };
+  },
+});
